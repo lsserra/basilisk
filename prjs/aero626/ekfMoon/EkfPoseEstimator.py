a3b73ac4b03970af7db5799a2f9195ad777768fe
Luke Serrano <lukeserrano0@gmail.com>
2025-10-26 11:35:56 -0500

    Pivot to impl MEKF and clean up EKF error state misunderstanding.

--- prjs/aero626/ekfMoon/EkfPoseEstimator.py
+++ prjs/aero626/ekfMoon/EkfPoseEstimator.py
@@ -130,14 +130,17 @@ class EkfPoseEstimator():
         self.MU_MOON = 4902.799 # km^3/s^3
 
 
-        self.H = np.hstack( (np.identity(3),np.zeros((3,3))) )
-
-
-
         # process noise and measurement noise matrices
         self.Qww = np.eye(self.nx)
         self.Fw = np.eye(self.nx)
+        
+
+
+        # --- Measurement Update Related --- #
+        self.Hx = np.hstack( (-np.identity(3),np.zeros((3,3))) )
+        self.Hv = np.eye(self.nz)
         self.Pvv = np.eye(self.nz)
+        self.landmarkMap = None
 
 
         # chi squared innovation position meas. gate, alpha=3, DOF=3
@@ -157,87 +160,10 @@ class EkfPoseEstimator():
         self.xRef_tk_ = xRef_tk_
         self.mx_prior_tk_ = mx_prior_tk_
 
-    def propagateReferenceState(self,toTime):
 
-        tk = toTime
-        # grab reference state
-        tk_ = self.xRef_tk_.t
-        xk_ = np.hstack([
-            self.xRef_tk_.r_BM_M,
-            self.xRef_tk_.Mdrdt_BM_M
-            ])
-        xk_ = xk_.flatten()
-        
+    def loadLandmarkMap(self,landmarkMapMCMF):
+        self.landmarkMap = landmarkMapMCMF
 
-        # numerically integrate
-        sol = solve_ivp(
-            t_span=[tk_,tk],
-            y0=xk_,
-            fun=lambda t, x: McmfPoseDynamics(t,x),
-            method='RK45',
-            rtol=1e-9,
-            atol=1e-9
-        )
-        xsol = sol.y
-        xk = xsol[:,-1]
-        
-        # write to tk ref solution 
-        self.xRef_tk.t = tk
-        self.xRef_tk.r_BM_M = xk[:3]
-        self.xRef_tk.Mdrdt_BM_M = xk[3:]
-
-    def propagateErrorCov(self,Fxk,toTime):
-
-        tk = toTime
-
-        # grab tk_ covariance
-        Pxx_tk_ = self.mx_prior_tk_.Pxx
-        tk_ = self.mx_prior_tk_.t
-        P0 = Pxx_tk_.flatten()
-        # numerically integrate
-        sol = solve_ivp(
-            t_span=[tk_,tk],
-            y0=P0,
-            fun=lambda t, P: CovDynamics(t, P, Fxk, self.Fw, self.Qww, self.nx),
-            method='RK45',
-            rtol=1e-9,
-            atol=1e-9
-        )
-        Pxx_tk = sol.y[:, -1].reshape((self.nx, self.nx))
-        
-        # set internal error state obj
-        self.mx_prior_tk.Pxx = Pxx_tk
-        self.mx_prior_tk.mx = np.zeros((self.nx,1))
-        self.mx_prior_tk.t = tk
-
-
-    def computeDynamicsJacobian(self):
-
-        # grab reference state
-        r = self.xRef_tk.r_BM_M
-        rdot = self.xRef_tk.Mdrdt_BM_M
-        rnormSquared = np.linalg.norm(r)**2
-
-        F11 = np.zeros((3,3))
-        F12 = np.eye(3)
-        F22 = -2*self.w_MN_M_skew
-
-        # gravitational partial wrt position
-        rnorm = np.linalg.norm(r)
-        I3 = np.eye(3)
-        F21_ = -self.MU_MOON * (I3 / rnorm**3 - 3 * np.outer(r, r) / rnorm**5)
-
-        # rdot partial wrt r
-        F21 = np.zeros((3,3))
-        for i in range(3):
-            for j in range(3):
-                F21[i,j] = (
-                    self.MU_MOON*( rdot[i]* rnormSquared**(-3/2) +
-                        r[i] * (-3*rnormSquared**(-5/2))* rdot[j] * r[j]
-                    ))
-        F = np.block([[F11,F12],
-                     [F21_,F22]]) 
-        return F        
 
     def propagate(self,toTime):
         ''' 
@@ -310,68 +236,31 @@ class EkfPoseEstimator():
 
 
 
-    def update(self, z_meas, measTime):
+    def updateWithLandmarks(self, z_meas_array, measTime):
         foo =1
-        '''
-        mxm = self.mxSol_mtk.mx
-        Pxxm = self.mxSol_mtk.Pxx
-
-        z = z_meas.reshape(-1, 1)
         
-        # Innovation
-        inn = z - self.H @ mxm
+        # get propagated error and reference state
+        dmx_prior_tk = self.mx_prior_tk.mx
+        Pxx_prior_tk = self.mx_prior_tk.Pxx
+        r_BM_M_tk = self.xRef_tk.r_BM_M
+        Mdrdt_BM_M = self.xRef_tk.Mdrdt_BM_M
+
         
-        # cross covariance
-        Pxzm = Pxxm @ self.H.T
-
-        # innovation covariance
-        Pzzm = (self.H @ Pxxm @ self.H.T) + self.Pvv
+        # shape measurement array
+        z = z_meas_array.reshape(-1, 3)
         
-        # Chi Squared Outlier Detection
-        if self.implChi2:
-            # perform outlier detection
-            # NIS should follow a chi squared distribution,
-            # inn ~ N(0,P), so nis ~ chi2
-            try:
-                Pzzm_inv = np.linalg.inv(Pzzm)
-            except np.linalg.LinAlgError:
-                # safeguard if Pzzm becomes singular
-                Pzzm_inv = np.linalg.pinv(Pzzm)
+        for i in range(len(z)):
 
-            nis = float(inn.T @ Pzzm_inv @ inn)
-            if not (nis < self.chi2gate_posmeas):
-                rejected = True
-                # solution is aprior solution
-                self.mxSol_ptk_ = self.mxSol_mtk
-                # Log data
-                self._log_state(mxm, Pxxm, measTime)
-                self._log_innovation(inn, measTime)
-                self._log_outlier(measTime, rejected)
-                return
-            
-            
-        rejected = False   
-        # kalman gain 
-        Kk = Pxzm @ np.linalg.inv(Pzzm)
+            # Innovation
+            lmId = z[i,-1]
+            r_LM_M = self.landmarkMap[lmId,:]
+            mzk = r_LM_M - r_BM_M_tk
+            zk = z[i,:]
+            inn =  - mzk
+        
 
-        # state update
-        mx_upd = mxm + Kk @ inn
-        # Update step
-        mx_upd = mxm + Kk @ inn
-        I = np.eye(self.nx)
-        Pxx_upd = (I - Kk @ self.H) @ Pxxm
 
-        # Store posterior state
-        self.mxSol_ptk.update(mx_upd, Pxx_upd, measTime)
-
-        # grab posteriori state and copy 
-        self.mxSol_ptk_ = self.mxSol_ptk
-
-        # Log data
-        self._log_state(mx_upd, Pxx_upd, measTime)
-        self._log_innovation(inn, measTime)
-        self._log_outlier(measTime, rejected)
-        '''
+'''
 
 
      # ---------------------------------------------------------
@@ -404,3 +293,4 @@ class EkfPoseEstimator():
         times = np.array([t for t, _ in self.outlier_log])
         rejected = np.array([flag for _, flag in self.outlier_log])
         return times, rejected
+'''
