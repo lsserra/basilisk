3a29868c20dc2bab4fea694c9e2b1d011cbd8723
Luke Serrano <lukeserrano0@gmail.com>
2025-10-26 13:46:10 -0500

    Implemented PosVel and MEKF states in separate state, due to decoupled dynamics.
     - Impl untested MEKF prop

--- prjs/aero626/ekfMoon/EkfPoseEstimator.py
+++ prjs/aero626/ekfMoon/EkfPoseEstimator.py
@@ -21,7 +21,7 @@ from helpers.attitude.Quaternion import Quaternion
 #           called by solve_ivp
 ############################################
 
-def EkfCoupledMeanAndCovProp(t, x_aug, MU_MOON, w_MN_M, Fw, Qww, nx):
+def EkfPosVelProp(t, x_aug, MU_MOON, w_MN_M, Fw, Qww, nx):
     """
     Coupled propagation of mean and covariance for EKF in MCMF frame.
     x_aug = [x, P_flat] 
@@ -72,26 +72,64 @@ def EkfCoupledMeanAndCovProp(t, x_aug, MU_MOON, w_MN_M, Fw, Qww, nx):
     return x_aug_dot
 
 
+def dqdt_wrapper(t, q, w_BM_B):
+    return Quaternion.dqdt(t, w_BM_B, q)
+
+def MekfCovProp(t,x_aug,nx,Fx,Fw,Qww):
+
+    P = x_aug.reshape((nx, nx))
+
+    # --- Covariance dynamics ---
+    Pdot = Fx @ P + P @ Fx.T + Fw @ Qww @ Fw.T
+
+    # --- Stack mean and covariance derivatives ---
+    x_aug_dot = Pdot.flatten()
+    
+    return x_aug_dot
+
+    
+
 
 ############################################
-# Classes to hold Error and Reference States
+# Classes to hold Different Filter States
 ############################################
-class EkfErrorState():
+class FullFilterState():
     def __init__(self,nx):
         self.mx = np.zeros((nx, 1))
         self.Pxx = np.eye(nx)
         self.t = 0.0
-    def update(self,x,P,t):
-        self.mx = x
-        self.Pxx = P
-        self.t = t
+    
+class EkfPosVelState():
+    def __init__(self,nx):
+        self.r_BM_M_mean = np.zeros((3,1))
+        self.Mdrdt_BM_M_mean = np.zeros((3,1))
+        self.Pxx = np.eye(nx)
 
-class EkfReferenceState():
-    def __init__(self):
-        self.r_BM_M = np.zeros((3,1))
-        self.Mdrdt_BM_M = np.zeros((3,1))
-        self.q_BM = Quaternion.identity()
-        self.t = 0
+        self.t = 0.
+
+class MekfState():
+    def __init__(self,nx):
+        # reference states
+        self.q_BMref = Quaternion.identity()
+        self.gyroBiasRef = np.zeros(3,1)
+        
+        # error states
+        self.angleError_mean = np.zeros(3,1)
+        self.gyroBiasError_mean = np.zeros(3,1)
+
+        # error cov
+        self.Pxx = np.eye(nx)
+
+        self.t = 0.
+
+
+class NoiseContatiner():
+    def __init__(QPosVel,QMekf,Pvv):
+
+    
+        
+
+
 
 
 
@@ -104,19 +142,46 @@ class EkfReferenceState():
 
 class EkfPoseEstimator():
 
-    def __init__(self, nx, nz):
+    def __init__(self):
+        
+        # define state sizes
+        self.nx_full = 12
+        self.nz = 3
 
-        self.nx = nx
-        self.nz = nz
+        self.nx_posVel = 6
+        self.nx_mekf = 6
 
-        # error state
-        self.mx_prior_tk_ = EkfErrorState(self.nx) # posteriori tk-1
-        self.mx_prior_tk = EkfErrorState(self.nx) # priori tk
-        self.mx_post_tk = EkfErrorState(self.nx) # posteriori tk
+        # full filter state
+        self.mx_full = FullFilterState(self.nx_full) 
+    
+        # position and velocity state
+        self.mx_posVel_prior_tk_ = EkfPosVelState(self.nx_posVel)
+        self.mx_posVel_prior_tk = EkfPosVelState(self.nx_posVel)
+        self.mx_posVel_post_tk = EkfPosVelState(self.nx_posVel)
 
-        # reference state
-        self.xRef_tk = EkfReferenceState()
-        self.xRef_tk_ = EkfReferenceState()
+        # MEKF state
+        self.mx_mekf_prior_tk_ = MekfState(self.nx_mekf)
+        self.mx_mekf_prior_tk = MekfState(self.nx_mekf)
+        self.mx_mekf_post_tk = MekfState(self.nx_mekf)
+
+        # Process Noise Shaping 
+        self.Fw_posVel = np.eye(self.nx_posVel)
+        self.Fw_mekf = block_diag(-np.eye(self.nx_mekf),np.eye(self.nx_mekf))
+
+        # PSD
+        self.QPosVel = np.zeros((self.nx_posVel,self.nx_posVel))
+        self.QMekf = np.zeros((self.nx_mekf,self.nx_mekf))
+
+        # --- Measurement Update Related --- #
+        self.Hx = np.hstack( (-np.identity(3),np.zeros((3,3))) )
+        self.Hv = np.eye(self.nz)
+        self.Pvv = np.eye(self.nz)
+        self.landmarkMap = None
+
+
+
+        
+
 
         # Moon angular rate
         self.w_MN_M = np.array([0.0, 0.0, 2*np.pi/27.322/24/3600])
@@ -128,44 +193,37 @@ class EkfPoseEstimator():
         ])
         # Moon gravitational const
         self.MU_MOON = 4902.799 # km^3/s^3
-
-
-        # process noise and measurement noise matrices
-        self.Qww = np.eye(self.nx)
-        self.Fw = np.eye(self.nx)
         
+       
 
-
-        # --- Measurement Update Related --- #
-        self.Hx = np.hstack( (-np.identity(3),np.zeros((3,3))) )
-        self.Hv = np.eye(self.nz)
-        self.Pvv = np.eye(self.nz)
-        self.landmarkMap = None
-
-
-        # chi squared innovation position meas. gate, alpha=3, DOF=3
-        self.chi2gate_posmeas = 7.815
-        self.implChi2 = False
 
         # --- Logging containers ---
-        self.errorState_log = []
-        self.refState_log = []
-        self.innovation_log = []  # stores (t, innovation)
-        
+        self.posVelState_log = []
+        self.mekfState_log = []
+        self.innovation_log = []  # stores (t, innovation)        
         self.outlier_log = []
 
 
 
-    def initialize(self,xRef_tk_: "EkfReferenceState" ,mx_prior_tk_: "EkfErrorState"):
-        self.xRef_tk_ = xRef_tk_
-        self.mx_prior_tk_ = mx_prior_tk_
+    def initialize(self,initPosVelState: "EkfPosVelState" ,initMekfState: "MekfState",
+                   QPosVel, Qmekf):
+        '''
+            Funtion to initialize mean position / velocity state, MEKF state, and PSD
+        '''
+        # initalize prior
+        self.mx_posVel_prior_tk_ = initPosVelState
+        self.mx_mekf_prior_tk_ = initMekfState
+
+        # PSD
+        self.QPosVel = QPosVel
+        self.QMekf = Qmekf
 
 
     def loadLandmarkMap(self,landmarkMapMCMF):
         self.landmarkMap = landmarkMapMCMF
 
 
-    def propagate(self,toTime):
+    def propagate(self,toTime,w_BM_B_meas):
         ''' 
             tk = toTime
 
@@ -181,15 +239,17 @@ class EkfPoseEstimator():
         tk = toTime
         
         # log error state and reference state before propagation
-        self.errorState_log.append(copy.deepcopy(self.mx_prior_tk_))
-        self.refState_log.append(copy.deepcopy(self.xRef_tk_))
+        self.posVelState_log.append(copy.deepcopy(self.mx_posVel_prior_tk_))
+        self.mekfState_log.append(copy.deepcopy(self.mx_mekf_prior_tk_))
 
         # grab prior error covariance, reference state, and time
-        tkm = self.mx_prior_tk_.t
-        Pxx_prior_tk_ = self.mx_prior_tk_.Pxx
+
+        # --- Pos Vel Propagation --- #
+        tkm = self.mx_posVel_prior_tk_.t
+        Pxx_prior_tk_ = self.mx_posVel_prior_tk_.Pxx
         xRef_tk_ = np.hstack([
-            self.xRef_tk_.r_BM_M,
-            self.xRef_tk_.Mdrdt_BM_M
+            self.mx_posVel_prior_tk_.r_BM_M_mean,
+            self.mx_posVel_prior_tk_.Mdrdt_BM_M_mean
             ]).flatten()
 
         x_aug0 = np.hstack([
@@ -199,14 +259,14 @@ class EkfPoseEstimator():
 
         # propagate the coupled mean and covariance dynamics
         sol = solve_ivp(
-        fun=lambda t, x_aug: EkfCoupledMeanAndCovProp(
+        fun=lambda t, x_aug: EkfPosVelProp(
             t,
             x_aug, 
             self.MU_MOON, 
             self.w_MN_M, 
-            self.Fw, 
-            self.Qww, 
-            self.nx
+            self.Fw_posVel, 
+            self.QPosVel, 
+            self.nx_posVel
             ),
         t_span=[tkm, tk],
         y0=x_aug0,
@@ -220,19 +280,89 @@ class EkfPoseEstimator():
         x_sol_tk = x_aug_sol[:self.nx, -1]
         Pxx_sol_tk = x_aug_sol[self.nx:, -1].reshape(self.nx,self.nx)
 
-        # update reference state obj
-        self.xRef_tk.r_BM_M = x_sol_tk[:3]
-        self.xRef_tk.Mdrdt_BM_M = x_sol_tk[3:]
-        self.xRef_tk.t = tk
-
-        # update error state obj
-        self.mx_prior_tk.Pxx = Pxx_sol_tk
-        self.mx_prior_tk.t = tk
+        # update pos vel state obj post propagation
+        self.mx_posVel_prior_tk.r_BM_M_mean = x_sol_tk[:3]
+        self.mx_posVel_prior_tk.Mdrdt_BM_M_mean = x_sol_tk[3:]
+        self.mx_posVel_prior_tk.t = tk
+        self.mx_posVel_prior_tk.Pxx = Pxx_sol_tk
 
 
-        # log error state and reference state after propagation
-        self.errorState_log.append(copy.deepcopy(self.mx_prior_tk))
-        self.refState_log.append(copy.deepcopy(self.xRef_tk))
+        # --- MEKF Propagation --- #
+        # ensure time is aligned with pos/vel
+        tkm_mekf = self.mx_mekf_prior_tk_.t
+        if not np.isclose(tkm,tkm_mekf,1e-8):
+            raise ValueError("Propagation tk minus for pos/vel and MEKF states do not align.")
+
+        # error states are zero, error has been added to nominal
+
+        # gyro mean dynamics = 0
+
+        # propagate quaternion
+        w_BM_B_corrected = w_BM_B_meas - self.mx_mekf_prior_tk_.gyroBiasRef
+        q_BM_tk_ = self.mx_mekf_prior_tk_.q_BMref.as_array()
+
+        sol = solve_ivp(
+        fun=lambda t,x : dqdt_wrapper(t,x,w_BM_B=w_BM_B_corrected),
+        t_span=[tkm, tk],
+        y0=q_BM_tk_,
+        method='RK45',
+        rtol=1e-9,
+        atol=1e-9
+        )
+
+        q_MN_tk = sol.y[:, -1]
+        q_MN_tk_obj = Quaternion.from_array(q_MN_tk).normalize()
+        
+        # propagate error covarance
+        wx,wy,wz = w_BM_B_corrected
+        omega_skew = np.array([
+            [0, -wz, wy],
+            [wz, 0, -wx],
+            [-wy, wx, 0]
+        ])
+        
+        FxMekf = np.array([
+            [omega_skew,-np.eye(3)],
+            np.zeros((3,6))])
+        
+        PxxFlat = self.mx_mekf_prior_tk_.Pxx.flatten()
+        
+        sol = solve_ivp(
+        fun=lambda t,x : MekfCovProp(t,x,
+                        nx=self.nx_mekf,
+                        Fx=FxMekf,
+                        Fw=self.Fw_mekf,
+                        Qww=self.QMekf),
+        t_span=[tkm, tk],
+        y0=PxxFlat,
+        method='RK45',
+        rtol=1e-9,
+        atol=1e-9
+        )
+        # reshape cov
+        x_aug_sol = sol.y 
+        PxxMekf_tk = x_aug_sol[:, -1].reshape(self.nx_mekf,self.nx_mekf)
+
+        # update mekf prior state obj at end of prop
+        self.mx_mekf_prior_tk.t = tk
+        self.mx_mekf_prior_tk.q_BMref = q_MN_tk_obj
+        self.mx_mekf_prior_tk.Pxx = PxxMekf_tk
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # log states after propagation
+        self.posVelState_log.append(copy.deepcopy(self.mx_posVel_prior_tk))
+        self.mekfState_log.append(copy.deepcopy(self.mx_mekf_prior_tk))
 
 
 
@@ -242,8 +372,8 @@ class EkfPoseEstimator():
         # get propagated error and reference state
         dmx_prior_tk = self.mx_prior_tk.mx
         Pxx_prior_tk = self.mx_prior_tk.Pxx
-        r_BM_M_tk = self.xRef_tk.r_BM_M
-        Mdrdt_BM_M = self.xRef_tk.Mdrdt_BM_M
+        r_BM_M_tk = self.xRef_tk.r_BM_M_mean
+        Mdrdt_BM_M = self.xRef_tk.Mdrdt_BM_M_mean
 
         
         # shape measurement array
