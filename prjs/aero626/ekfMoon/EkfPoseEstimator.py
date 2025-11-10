Negative covariance in off diagnal terms is fine. Believed that Jacobians are correct.
 - Running simulations with gyro and map noise.

 - Kalman Gain clamping down and destroying filter depending on Pvv

prjs/aero626/ekfMoon/EkfPoseEstimator.py
@@ -299,6 +299,11 @@ class EkfPoseEstimator():
         self.mx_posVel_prior_tk.Pxx = Pxx_sol_tk
 
 
+         # if any non-finite values, skip update
+        if np.any(Pxx_sol_tk[np.diag_indices_from(Pxx_sol_tk)] < 0.0):
+            self._log_outlier(tk, True)
+            
+
         # --- MEKF Propagation --- #
         # ensure time is aligned with pos/vel
         tkm_mekf = self.mx_mekf_prior_tk_.t
@@ -342,11 +347,6 @@ class EkfPoseEstimator():
         
 
         PxxFlat = self.mx_mekf_prior_tk_.Pxx.flatten()
-
-         # if any non-finite values, skip update
-        if np.any(PxxFlat < 0.0):
-            self._log_outlier(tk, True)
-            return
         
         sol = solve_ivp(
         fun=lambda t,x : MekfCovProp(t,x,
@@ -364,11 +364,12 @@ class EkfPoseEstimator():
         x_aug_sol = sol.y 
         PxxMekf_tk = x_aug_sol[:, -1].reshape(self.nx_mekf,self.nx_mekf)
 
+        
 
-         # if any non-finite values, skip update
-        if np.any(PxxMekf_tk.flatten() < 0.0):
+          # if any non-finite values, skip update
+        if np.any(PxxMekf_tk[np.diag_indices_from(PxxMekf_tk)] < 0.0):
             self._log_outlier(tk, True)
-            return
+            
 
         # update mekf prior state obj at end of prop
         self.mx_mekf_prior_tk.t = tk
@@ -409,7 +410,7 @@ class EkfPoseEstimator():
 
             # get map landmark position
             map_r_LM_M = self.landmarkMap[landmarkIds[i],:].flatten()
-            map_r_LM_B= self.mx_mekf_post_tk.q_BMref.rotate(
+            map_r_LM_B= self.mx_mekf_prior_tk.q_BMref.rotate(
                 map_r_LM_M
             ) 
 
@@ -429,14 +430,21 @@ class EkfPoseEstimator():
             # MEKF Hx with nx_fullstate columns
             # skew of mean of mcmf to body frame position
             HxMekf = np.zeros((self.nz,self.nx_mekf))
-            r_LB_B = self.mx_mekf_prior_tk.q_BMref.rotate(map_r_LM_M - mr_BM_M_tk)
-            rx,ry,rz = r_LB_B
-            r_LB_B_skew = np.array([
+            rx,ry,rz = TBMhat@map_r_LM_M
+            map_r_LM_B_skew = np.array([
                 [0, -rz, ry],
                 [rz, 0, -rx],
                 [-ry, rx, 0]
             ])
-            HxMekf[:,:3] = r_LB_B_skew
+            rx,ry,rz = TBMhat@mr_BM_M_tk
+            mr_BM_B_tk_skew = np.array([
+                [0, -rz, ry],
+                [rz, 0, -rx],
+                [-ry, rx, 0]
+            ])
+            HxMekf[:,:3] = map_r_LM_B_skew - mr_BM_B_tk_skew
+
+            
 
             if HxStack is not None:
                 HxStack = np.vstack((HxStack,
@@ -447,14 +455,13 @@ class EkfPoseEstimator():
                 innovationsVec = np.vstack((innovationsVec, innovation.reshape(-1,1)))
             else:
                 HxStack = np.hstack((HxTrans,HxMekf))
-                Pxxk_prior_block = self.mx_full.Pxx
                 PvvStack = self.Pvv
                 innovationsVec = innovation.reshape(-1,1)
                 
         # prepare for kalman update
         Pxxk_prior = self.mx_full.Pxx
         Pxzk = Pxxk_prior @ HxStack.T
-        Pzzk = HxStack @ Pxxk_prior @ HxStack.T + PvvStack
+        Pzzk = (HxStack @ Pxxk_prior @ (HxStack.T)) + PvvStack
         Kk = Pxzk @ linalg.inv(Pzzk)
 
         # store innovation covariance
@@ -471,7 +478,7 @@ class EkfPoseEstimator():
         
         # kalman update
         mxk_post = mxk_prior + Kk @ innovationsVec
-        Pxxk_post = Pxxk_prior - Pxzk@Kk.T -Kk@Pxzk.T + Kk @ Pzzk @ Kk.T
+        Pxxk_post = Pxxk_prior - Pxzk@Kk.T - Kk@Pxzk.T + Kk @ Pzzk @ Kk.T
 
         # unpack updated states
         self.mx_posVel_post_tk.r_BM_M_mean = mxk_post[:3].flatten()
@@ -486,9 +493,9 @@ class EkfPoseEstimator():
         self.mx_full.Pxx = Pxxk_post
 
          # if any non-finite values, skip update
-        if np.any(self.mx_full.Pxx.flatten() < 0.0):
+        if np.any(self.mx_full.Pxx[np.diag_indices_from(self.mx_full.Pxx)] < 0.0):
             self._log_outlier(measTime, True)
-            return
+            
 
         # add attitude error correction to nominal quaternion
         q_err = Quaternion(qv=self.mx_mekf_post_tk.angleError_mean.flatten(),

prjs/aero626/ekfMoon/scenarioEkfMoon.py
@@ -153,14 +153,18 @@ def run(showPlots, savePkl, EarthAndMoonGrav):
     
     # Configure gyro noise (rad/s)
     # imu.senRotBias = np.array([0.,0.,0.])
-    senRotNoiseStd = 0.0000 # rad/s
+    senRotNoiseStd = np.sqrt(10)*10**(-7) # rad/sec^(3/2)
     walkBound = 0.01 # rad/s
     PMatrix = np.eye(3)* senRotNoiseStd**2 # cholesky defactorization of noise covariance matrix, drives Gauss Markov Process
-    # L = np.linalg.cholesky(PMatrix)
-    L = np.zeros((3,3))
+    L = np.linalg.cholesky(PMatrix)
+    # L = np.zeros((3,3))
     imu.PMatrixGyro = L
     AMatrixGyro = np.zeros((3,3))
-    imu.AMatrixGyro = AMatrixGyro
+
+    senWalkNoiseStd = np.sqrt(10)*10**(-10) # rad/sec^(3/2)
+    AMatrixGyro = np.eye(3)* senWalkNoiseStd**2 # cholesky defactorization of noise covariance matrix, drives Gauss Markov Process
+    L = np.linalg.cholesky(AMatrixGyro)
+    imu.AMatrixGyro = L
     imu.setErrorBoundsGyro([walkBound, walkBound, walkBound])
     imu.scStateInMsg.subscribeTo(scObject.scStateOutMsg)
     # Add IMU to simulation

prjs/aero626/ekfMoon/unitTestEkf.py
@@ -42,7 +42,8 @@ print("Simulation data successfully unboxed.")
 
 ## limit sim time for testing ##
 # idxCap = 25
-idxCap = 700
+# idxCap = 700
+idxCap = 1500
 if idxCap is not None:
     timeData = timeData[:idxCap]
     sc_pos = sc_pos[:idxCap,:]
@@ -131,8 +132,8 @@ nz = 3
 # ekf object
 ekf = EkfPoseEstimator()
 # Process Noise
-psd_r = 1. # 
-psd_Mdrdt = 0.1 # 
+psd_r = .1 # 
+psd_Mdrdt = 0.01 # 
 Qww_posVel = np.diag([psd_r,psd_r,psd_r,psd_Mdrdt,psd_Mdrdt,psd_Mdrdt])
 
 
@@ -169,13 +170,14 @@ q_BM_true_0 = q_BN_0*q_NM_0
 mekfState0.q_BMref = q_BM_true_0
 
 # covariance
-# sigmaAtt = np.deg2rad(5.) # deg -> rad
-# sigmaGyroBias = np.deg2rad(.1) # deg/s -> rad/s
-
-sigmaAtt = 9.4e-6 # rad^2
-sigmaGyroBias = 9.4e-13 # rad^2/s^2
+sigmaAtt = np.deg2rad(.1) # deg -> rad
+sigmaGyroBias = np.deg2rad(.2)/3600 # deg/hr -> rad/s
 Pxx0 = block_diag(sigmaAtt*np.eye(3),sigmaGyroBias*np.eye(3))
-# Pxx0 = Pxx0@Pxx0.T
+Pxx0 = Pxx0@Pxx0.T
+
+# sigmaAtt = 9.4e-6 # rad^2
+# sigmaGyroBias = 9.4e-13 # rad^2/s^2
+# Pxx0 = block_diag(sigmaAtt*np.eye(3),sigmaGyroBias*np.eye(3))
 mekfState0.Pxx = Pxx0
 
 # time
@@ -183,7 +185,7 @@ mekfState0.t = t0
 
 # process noise
 
-Qmekf = 0.0000 * np.eye(6) # TODO check!
+Qmekf = 1e-12 * np.eye(6) # TODO check!
 
 
 # pass IC's, process noise PSD to filter
@@ -195,13 +197,13 @@ ekf.initialize(
 
 ## landmark measurement initialization ##
 # measurement noise
-oneSigmaLandmarkMeas = 25 # km
+oneSigmaLandmarkMeas = 5 # km
 PvvLM = oneSigmaLandmarkMeas**2 * np.eye(3)
 ekf.Pvv = PvvLM
 # feed map 
 # ekf.loadLandmarkMap(mapLandmarks) 
-ekf.loadLandmarkMap(trueLandmarks) # TODO WARNING passing map = truth
-
+# ekf.loadLandmarkMap(trueLandmarks) # TODO WARNING passing map = truth
+ekf.loadLandmarkMap(mapLandmarks)
 
 
 
@@ -370,6 +372,8 @@ print(f"Final v Error = {velocityError[-1,:]} [km/s]")
 t_filt = np.array([s.t for s in mekfStateList])
 Pxx_list = [s.Pxx for s in mekfStateList]
 P_diag_mekf = np.array([np.diag(P) for P in Pxx_list])
+# convert to deg
+P_diag_mekf = np.rad2deg(np.rad2deg(P_diag_mekf))
 sigma3_mekf = 3 * np.sqrt(P_diag_mekf)
 
 
@@ -452,8 +456,8 @@ gryo_labels = ['X', 'Y', 'Z']
 # Position error plots
 for i in range(3):
     axs[i, 0].plot(t_filt, np.rad2deg(PRV_BprimeB_array[:, i]), 'k-', linewidth=1.8, label=f'{body_labels[i]}')
-    axs[i, 0].plot(t_filt, np.rad2deg(sigma3_mekf[:, i]), 'r--', linewidth=1)
-    axs[i, 0].plot(t_filt, np.rad2deg(-sigma3_mekf[:, i]), 'r--', linewidth=1, label='±3σ confidence')
+    axs[i, 0].plot(t_filt, (sigma3_mekf[:, i]), 'r--', linewidth=1)
+    axs[i, 0].plot(t_filt, (-sigma3_mekf[:, i]), 'r--', linewidth=1, label='±3σ confidence')
     axs[i, 0].set_ylabel(f'Body Frame {body_labels[i]} Error [deg]')
     axs[i, 0].grid(True)
     axs[i, 0].legend(loc='upper right')
@@ -462,8 +466,8 @@ for i in range(3):
 # Velocity error plots
 for i in range(3):
     axs[i, 1].plot(t_filt, np.rad2deg(gyroBiasError_array[:,i]), 'k-', linewidth=1.8, label=f'{body_labels[i]}')
-    axs[i, 1].plot(t_filt, np.rad2deg(sigma3_mekf[:, 3 + i]), 'r--', linewidth=1)
-    axs[i, 1].plot(t_filt, np.rad2deg(-sigma3_mekf[:, 3 + i]), 'r--', linewidth=1,label='±3σ confidence')
+    axs[i, 1].plot(t_filt, (sigma3_mekf[:, 3 + i]), 'r--', linewidth=1)
+    axs[i, 1].plot(t_filt, (-sigma3_mekf[:, 3 + i]), 'r--', linewidth=1,label='±3σ confidence')
     axs[i, 1].set_ylabel(f'Gyro Frame {body_labels[i]} Bias Error [deg/s]')
     axs[i, 1].grid(True)
     axs[i, 1].legend(loc='upper right')